1acad169e9b6db70a010bb62f49e708b3af45611
JieGG <947295340@qq.com>
2019-05-21 11:59:20 +0800

    更新日志

--- Native.Basic/My Project/AssemblyInfo.vb
+++ Native.Basic/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 方法是按如下所示使用“*”: :
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.0.3.0517")>
-<Assembly: AssemblyFileVersion("2.0.3.0517")>
+<Assembly: AssemblyVersion("2.0.4.0521")>
+<Assembly: AssemblyFileVersion("2.0.4.0521")>
